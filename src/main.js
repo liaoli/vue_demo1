6298771f79e9045541cf8eddad958b5c6ea8ab33
liaoli <277218962@qq.com>
2023-09-12 16:38:15 +0800

    全局组件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HmButton from'./components/HmButton.vue'
 
 Vue.config.productionTip = false
+Vue.component('HmButton',HmButton)
 
 new Vue({
   el: '#app',
